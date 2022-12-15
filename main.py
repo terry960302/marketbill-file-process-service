88f1b620f510727b2c9823cef2dce953d17a4daf
Taewan Kim <terry960302@gmail.com>
2022-12-15 22:50:26 +0900

    :sparkles: #5 처리 작업별 프로세스 분리 작업

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 from fastapi import FastAPI, Request
 from controllers.root_controller import health_check
 from controllers.process_controller import handle_receipt_process
+from concurrent.futures.process import ProcessPoolExecutor
+import asyncio
 
 app = FastAPI()
+
+
+async def run_in_process(fn, *args):
+    loop = asyncio.get_event_loop()
+    return await loop.run_in_executor(app.state.executor, fn, *args)  # wait and return result
+
+
+@app.on_event("startup")
+async def on_startup():
+    app.state.executor = ProcessPoolExecutor()
+
+
+@app.on_event("shutdown")
+async def on_shutdown():
+    app.state.executor.shutdown()
 
 
 @app.get("/")
@@ -13,4 +30,6 @@ def handle_root():
 @app.post("/")
 async def handle_receipt(request: Request):
     json_object = await request.json()
-    return handle_receipt_process(json_object)
+    return await run_in_process(handle_receipt_process, json_object)
+
+    # return handle_receipt_process(json_object)
